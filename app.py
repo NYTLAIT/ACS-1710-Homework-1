a557c499bde8d92a13ee93da77bdef305fa79ef3
NYTLAIT <231107618+NYTLAIT@users.noreply.github.com>
2025-09-15 15:29:26 -0700

    Stuck on sayntimes, cannot figure out how to delete space at the end

--- app.py
+++ app.py
@@ -1,3 +1,48 @@
 
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    """Shows a greeting to the user."""
+    return 'Are you there, world? It\'s me, Ducky!'
+
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+    """Display a message to the user that changes based on their favorite animal."""
+    return f'Wow, {users_animal} is my favorite animal, too!'
+
+@app.route('/dessert/<users_dessert>')
+def favorite_dessert(users_dessert):
+    return f'How did you know I liked {users_dessert}?'
+
+@app.route('/madlibs/<adjective>/<noun>')
+def madlibs(adjective, noun):
+    return f'The {adjective} {noun} yelled at everyone in the fridge.'
+
+@app.route('/multiply/<number1>/<number2>')
+def multiply(number1, number2):
+    if number1.isdigit() and number2.isdigit():
+        product = int(number1) * int(number2)
+        return f'{number1} times {number2} is {product}'
+    else:
+        return f'Invalid inputs. Please try again by entering 2 numbers!'
+
+@app.route('/sayntimes/<word>/<n>')
+def sayntimes(word, n):
+    if n.isdigit():
+        words = " "
+        words += word
+        for i in range(int(n)):
+            if i < int(n) - 1:
+                words += word + " "
+        return f'{words}'
+    else:
+        return f'Invalid input. Please try again by entering a word and a number!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
